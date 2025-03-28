7f3e49afde36fd19ba8d959092c1e91497789a5d
HardyJL <j.hardenberg@hotmail.de>
2025-03-28 10:19:08 +0100

    feat: csv for a;;

--- csv/csv_functions.py
+++ csv/csv_functions.py
@@ -1,26 +1,12 @@
 import csv
 
-from ..models import Task, Solution
-
-def load_tasks_from_csv(filename) -> list[Task]:
-    """
-    Loads tasks from csv file
-
-    Args:
-        filename string: path to csv file
-
-    Returns:
-        List of tasks
-    """
-    tasks = []
+def load_models_from_csv(filename) -> list:
+    models = []
     try:
         with open(filename, newline='') as csvfile:
             reader = csv.reader(csvfile)
             next(reader)  # Skip header
             for row in reader:
-                tasks.append(Task(*row))
+                models.append(*row)
     except Exception as e:
         print(f"Error reading CSV file: {e}")
-        exit(1)
-    return tasks
-
