6b4a35dac5fc08c34f89ecd1b5b5f6f85d5f5241
olivier <olivier.jarrosson@gmail.com>
2025-04-04 10:05:54 +0200

    mapping
    Co-authored-by: Johannes Hardenberg <HardyJL@users.noreply.github.com>

--- Simulation/simulation.py
+++ Simulation/simulation.py
@@ -9,7 +9,24 @@ import os.path
 def simulation(test_folder:str,max_cycles:int):
     #Use csv_function to initialize architecture, budgets and task
     architecture, budgets, tasks=csvs.csv_functions.load_models_from_csv(test_folder)
-    print(architecture)
-    print(budgets)
-    print(tasks)
-    #architecture= 
+    # print(architecture)
+    # print(budgets)
+    # print(tasks)
+    current_cycle = 0 
+
+    core_assignment={}
+    for i in architecture:
+        core_assignment[i.core_id] = {} 
+    for i in budgets:
+        core_assignment[i.core_id][i.component_id] = []
+        for j in tasks:
+            if j.component_ID==i.component_id:
+                core_assignment[i.core_id][i.component_id].append(j)
+    print(core_assignment)
+
+
+
+    while current_cycle <= max_cycles:
+
+
+        break
